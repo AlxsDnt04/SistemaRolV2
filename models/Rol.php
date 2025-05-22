42b186940fd811404a2a489d123f669d93148505
AlxsD <carapedoxd@gmail.com>
2025-05-22 10:55:49 -0500

    Implement guardar method in Rol model to insert employee data into the database

--- models/Rol.php
+++ models/Rol.php
@@ -16,5 +16,44 @@ class Rol
         return $this->db->query("SELECT * from empleado")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-
+    // funcion para insertar los datos en la base
+    public function guardar($data)
+    {
+        $stat = $this->db->prepare("INSERT INTO rol ( 
+        mes, 
+        hora25,
+        hora50,
+        hora100,
+        bonos,
+        sueldo,
+        totalIngreso,
+        iess,
+        multas,
+        atrasos,
+        alimentacion,
+        anticipos,
+        otros,
+        totalEgreso,
+        totalPagar,
+        ci_empleado
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        return $stat->execute([
+            $data['mes'], // directo del formulario
+            $data['hora25'],
+            $data['hora50'],
+            $data['hora100'],
+            $data['bonos'], //directo del formulario
+            $data['sueldo'], //directo del formulario
+            $data['totalIngreso'],
+            $data['iess'],
+            $data['multas'], //directo del formulario
+            $data['atrasos'], //directo del formulario
+            $data['alimentacion'], //directo del formulario
+            $data['anticipo'], //directo del formulario
+            $data['otros'], //directo del formulario
+            $data['totalEgreso'],
+            $data['totalPagar'],
+            $data['empleadoInfo'] //directo del formulario
+        ]);
+    }
 }
